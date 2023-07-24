3b230551c69d628cf08c8ae6efb18306fde6f8e1
Rodrigo Oliveira <rodrigossooliveira19@gmail.com>
2023-07-24 12:22:26 -0300

    Initial commit

---
+++ Estudos Js/Módulo 1/Exercicios/JS/script.js
@@ -0,0 +1,42 @@
+//1. Crie uma variável para amarzenar o nome do carro e ponha o valor Ferrari. E exiba-a
+let carro = "Ferrari"
+console.log(carro)
+
+//2. Crie uma variável para armazenar o preço do bolo, que custa R$ 19,99. E exiba-a
+let preco = 19.99
+console.log(preco)
+
+//3. Corrija o código a seguir (e exiba-o). let "cidade" = São Paulo
+let cidade = "São Paulo"
+console.log(cidade)
+
+
+//4. Substitua o @ pela condicional correta para que o resultado seja TRUE
+let x = 10;
+let y = 5;
+//console.log(x @ y);
+console.log(x > y);
+
+
+//5. Substitua o @ pela condicional correta para que o resultado seja FALSE
+let w = "10";
+let z = 10;
+//console.log(w @ z);
+console.log(w === z);
+
+
+//6. Substitua o @ pela condicional correta para que o resultado seja TRUE
+let a = 10;
+let b = 5;
+//console.log(x @ y);
+console.log(a != b);
+
+
+//7. Crie uma condicional para verificar se o preço da carne está barato ou caro. PS: Até 45 está barato.
+let precocarne = 40.3;
+
+if (precocarne > 45) {
+    console.log("Está caro")
+}  else{ 
+    console.log("Está barato")
+}
---
+++ Estudos Js/Módulo 1/assets/js/script.js
@@ -0,0 +1,148 @@
+alert("olá")
+
+let nome= "Rodrigo";      //string
+let idade= 20;            //number
+let cidade= "Salvador";   //string
+let estado= "Bahia";      //string
+
+console.log("Meu nome é " + nome);
+console.log("Tenho " + idade + " anos");
+console.log("Moro em " + cidade);
+console.log("Na " + estado);
+
+
+
+/* let logado = true       *boolean* 
+   let logado = false      *boolean* 
+
+ let lista = [ovo, leite, acucar]  *array*
+ let nomecompleto = {nome: "Rodrigo", idade: 20}  *object* 
+*/
+
+/* Tipos de Variáveis
+ let - Variável que aceita qualquer valor
+ var - Variável que aceita qualquer valor
+ const - Variável constante, não muda
+*/
+
+
+// Operações matemáticas
+let x = 2;
+x = x + 20;
+console.log(x)
+
+
+// Concatenar
+let name = "Luiz"
+let sobrenome = "Santos";
+let nomecompleto= (name + " " + sobrenome)
+
+console.log(nomecompleto)
+
+
+
+// template string
+let nomes = "Julio";
+let sobrenomes = "Silva";
+
+//let nomescompleto = (nomes + " " + sobrenomes);
+let nomescompleto = `${nomes} ${sobrenomes} Santos`
+
+console.log(nomescompleto)
+
+
+
+// If / Else
+    let idades = 19; 
+/*
+    if( idades > 17) {
+        console.log("Você é MAIOR de idade")    
+    }  else{
+        console.log("Você é MENOR de idade")
+    }
+*/
+
+/*if (idades >=18) {
+        if (idades <60) {
+            console.log("Você é um adulto")
+        }
+    }
+*/
+
+if (idades >= 18 && idades <=60) {
+    console.log("Você é um adulto")
+}
+
+
+let n1 = 8;
+let n2 = 6;
+let n3 = 4;
+
+let media = (n1 + n2 + n3) / 3;
+
+if (media <= 4) {
+    console.log("Média = ", media, "(Reprovado!)");
+} else if (media < 7) {
+    console.log("Média = ", media, "(Recuperação!)");
+} else {
+    console.log("Média = ", media, "(Aprovado)");
+}
+
+
+/*Simbolos
+< Menor que
+> Maior que
+== Igual
+=== Igual - restritivo
+>= Maior ou igual
+<= Menor ou igual
+!= Diferente
+&& E
+|| Ou
+*/
+
+
+
+//Condicional Ternario
+let Prime = true;
+
+let shipping = Prime ? "Grátis" : 20;
+
+console.log(Prime ? 'Prime' : 'Não é Prime');
+console.log ("Frete: " + shipping);
+
+
+
+let age = 90;
+
+let isAdult = ((age >=18) ? 'Sim' : 'Não');
+
+console.log(isAdult);
+
+
+
+
+
+
+
+
+
+// Switch
+let profession = "bombeiro";
+
+console.log("Profissão: " + profession);
+
+switch (profession) {
+    case "fiscal":
+        console.log ("Sua camisa será VERDE");
+        break;
+    case "bombeiro":
+        console.log ("Sua camisa será VERMELHA")
+        break;
+    case "policial":
+        console.log ("Sua camisa será AZUL");
+        break;
+    default:
+        console.log ("Sua Camisa será PRETA")
+        break;
+}
