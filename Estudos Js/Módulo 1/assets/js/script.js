c1d1ff18d8083aa2498abf6f36cfe00c0566ed55
Rodrigo Oliveira <140333448+RodrigoSSOliveira@users.noreply.github.com>
2024-02-28 20:48:06 -0300

    Update script.js

--- Estudos Js/Módulo 1/assets/js/script.js
+++ Estudos Js/Módulo 1/assets/js/script.js
@@ -144,6 +144,6 @@ switch (profession) {
 
 //Funções
 function gravidade(){
-    console.log("A gravidade do planeta é:")
+    console.log("A gravidade do planeta Terra é:")
     console.log(9.8)
 }
